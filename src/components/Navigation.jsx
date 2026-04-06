5a5f28c4eec390effbb25c280f2db404ed3288cb
Dev Mandal <devmandalgps@gmail.com>
2026-04-06 20:38:17 +0530

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,4 @@
 
-// import React from 'react'
-// import { useNavigate, useLocation } from 'react-router-dom'
-
-// // Navigation component that moves through an ordered list of routes.
-// // - `routes` prop: ordered array of valid route paths (e.g. ['/', '/about', '/contact']).
-// // - Previous / Next move within that array; Home always 
-
-//   // Determine if there is a previous/next route inside the provided routes array
-//   const hasPrevious = idx > 0
-
-
-//   const baseBtn = 'px-4 py-2 rounded font-medium transition'
 //   const enabled = 'bg-gray-800 text-white hover:bg-gray-700'
 //   const disabled = 'opacity-50 cur
 import React from 'react'
